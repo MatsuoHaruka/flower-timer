529dcb3b7dcbcc20515b3ff02cc408232f798855
Matsuo Haruka <springflower.kusky@icloud.com>
2015-08-05 17:21:32 +0900

    修正、サウンドの追加

--- ex7/ex7/ViewController.swift
+++ ex7/ex7/ViewController.swift
@@ -7,47 +7,25 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
     @IBOutlet weak var myImageView: UIImageView!
-
     @IBOutlet weak var myPickerView: UIPickerView!
     @IBOutlet weak var myLabel: UILabel!
-    
-    @IBAction func myButton(sender: UIButton) {
-        
-       //タイマーが動いてないとき
-        if timer.valid == false{
-            timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
-            
-            sender.setTitle("諦める", forState: UIControlState.Normal)
-            self.myImageView.image = UIImage(named: "flower.png")
-            
-            startTime = NSDate()
-            
-            defaults.setObject(startTime, forKey:"startTime")
-            defaults.synchronize()
-            println(startTime)
-        
-        //タイマーが動いてるとき
-        }else{
-            timer.invalidate()
-            sender.setTitle("始める", forState: UIControlState.Normal)
-            self.myImageView.image = UIImage(named: "flower2.png")
-            second = 0
-        }
-        
-    }
-  
+    @IBOutlet weak var myButton2: UIButton!
+    @IBOutlet weak var myButton: UIButton!
     
     
     var second = 0
     var timer : NSTimer!
     let defaults = NSUserDefaults.standardUserDefaults()
     var startTime : NSDate!
-    var time : NSDate!
+    var time :Int!
+    var settingTime = 60
     var now = String()
-    var list = ["1","5","10","20","30"]
+    var list = ["1:00","5:00","10:00","20:00","30:00","テスト"]
+    var listTime = "1:00"
     
     
     override func viewDidLoad() {
@@ -56,6 +34,10 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
         
         self.myPickerView.delegate = self
         self.myPickerView.dataSource = self
+        
+        self.myButton2.hidden = true
+        
+        myButton.addTarget(self, action: "touchUpButton:", forControlEvents:.TouchUpInside)
         
         timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
         timer.invalidate()
@@ -86,7 +68,6 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
         if timer.valid == true {
             let restartTime = NSDate()
             
-//            startTime = defaults.objectForKey("startTime") as! NSDate
             var stopTime = defaults.objectForKey("stopTime") as! NSDate
          
             var interval = restartTime.timeIntervalSinceDate(stopTime)
@@ -100,15 +81,14 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
         }
     }
     
-    
-    
     func update(){
         second++
         makeLabel()
     }
     
     func makeLabel(){
-        var time = 1800 - second
+        
+        time = settingTime - second
         var m = time / 60
         var s = time - 60*m
         
@@ -121,11 +101,67 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
         }
         self.myLabel.text = now
         
+        //計り終わったら
         if time == 0 {
             self.myImageView.image = UIImage(named: "chou.jpg")
-        }
+            timer.invalidate()
+            second = 0
+            self.myLabel.hidden = true//非表示
+            self.myButton.hidden = true
+            self.myButton2.hidden = false
+            myButton2.addTarget(self, action: "touchUpButton2:", forControlEvents:.TouchUpInside)
+            
+            //サウンド
+            var soundIdRing:SystemSoundID = 1009
+            AudioServicesCreateSystemSoundID(nil, &soundIdRing)
+            AudioServicesPlaySystemSound(soundIdRing)
+            }
 
     }
+    
+    //myButtonがタップされたら
+    func touchUpButton(sender:UIButton) {
+        
+        //タイマーが動いてないとき
+        if timer.valid == false{
+            self.myLabel.text = listTime
+            
+            timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+            
+            sender.setTitle("諦める", forState: UIControlState.Normal)
+            self.myImageView.image = UIImage(named: "flower.png")
+            
+            startTime = NSDate()
+            
+            defaults.setObject(startTime, forKey:"startTime")
+            defaults.synchronize()
+            println(startTime)
+            
+            self.myLabel.hidden = false//表示
+            self.myPickerView.hidden = true//非表示
+            
+            //タイマーが動いてるとき
+        }else if timer.valid == true{
+            timer.invalidate()
+            sender.setTitle("始める", forState: UIControlState.Normal)
+            self.myImageView.image = UIImage(named: "flower2.png")
+            second = 0
+            
+            self.myLabel.hidden = true//非表示
+            self.myPickerView.hidden = false//表示
+        }
+    }
+    
+    
+    //myButton2がタップされたら
+    func touchUpButton2(sender:UIButton) {
+        self.myImageView.image = UIImage(named: "flower.png")
+        self.myPickerView.hidden = false
+        self.myButton2.hidden = true
+        self.myButton.hidden = false
+        self.myButton.setTitle("始める", forState: UIControlState.Normal)
+    }
+
     
     //Picker
     func numberOfComponentsInPickerView(pickerview1: UIPickerView) -> Int {
@@ -139,8 +175,28 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
         return list[row]
     }
     func pickerView(pickerview1: UIPickerView, didSelectRow row: Int, inComponent component: Int){
-        self.myLabel.text = list[row]
+        if row == 0 {
+            self.settingTime = 60
+            self.listTime = "1:00"
+        }else if row == 1 {
+            self.settingTime = 300
+            self.listTime = "5:00"
+        }else if row == 2 {
+            self.settingTime = 600
+            self.listTime = "10:00"
+        }else if row == 3 {
+            self.settingTime = 1200
+            self.listTime = "20:00"
+        }else if row == 4 {
+            self.settingTime = 1800
+            self.listTime = "30:00"
+        }else if row == 5 {
+            self.settingTime = 15
+            self.listTime = "0:15"
+        }
+
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
